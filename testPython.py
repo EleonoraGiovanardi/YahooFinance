fb621036e657e69a6de536d514a08ca25024142b
eleonora <elly.giova@gmail.com>
2021-06-03 15:54:49 +0200

    aggiunta azioni MSFT

--- testPython.py
+++ testPython.py
@@ -8,3 +8,11 @@ dati.to_csv("AzioniApple10anni.csv")
 
 print("Applicazione che si connette a YF per scaricare i dati di Apple")
 print(dati.head())
+
+t = yf.Ticker("MSFT")
+
+dati_m = t.history(period = "10Y")
+dati_m.to_csv("AzioniMSFT10anni.csv")
+
+print("Applicazione che si connette a YF per scaricare i dati di MSFT")
+print(dati.head())
